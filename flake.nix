56e7bf979424451d0b02458b60283819b80ffdbc
Ryan Hawkins <ryan.hawkins@instructure.com>
2025-07-30 12:47:43 -0600

    Refine configuration

--- flake.nix
+++ flake.nix
@@ -56,24 +56,49 @@
       home.stateVersion = "25.05";
       # Let home-manager install and manage itself.
       programs.home-manager.enable = true;
+      home.shell.enableFishIntegration = true;
 
       home.packages = with pkgs; [
         pkgs.neovim
-        pkgs.nodejs_24
+        pkgs.zed-editor
+        pkgs.fzf
+        pkgs.ripgrep
+        pkgs.devenv
       ];
 
       home.sessionVariables = {
         EDITOR = "nvim";
       };
 
+      programs.direnv = {
+          enable = true;
+      };
+
+      programs.zoxide = {
+          enable = true;
+      };
+
       programs.fish = {
         enable = true;
         shellAliases = {
           open-nix-config = "$EDITOR ${CONFIG_FILE_PATH}/flake.nix";
+          g = "git";
+          gri = "git rebase --interactive";
+          grh = "git reset --hard";
+          reload = "source ~/.config/fish/config.fish";
+          fishrc = "$EDITOR ~/.config/fish/config.fish";
+          vimrc = "$EDITOR ~/.config/nvim";
+          dcr = "docker compose run --rm";
+          dex = "docker compose exec";
+          dc = "docker compose";
+          lg = "lazygit";
+          grc = "git rebase --continue";
+          gca = "git commit --amend";
         };
         functions = {
           rebuild-nix-conf = ''
             echo "🔧 About to rebuild nix-darwin configuration..."
+            echo "This will require sudo access."
             read -l reply -p "set_color green; echo -n 'Are you sure you want to proceed? [y/N]: '; set_color normal"
             switch (string lower "$reply")
               case "y" "yes"
